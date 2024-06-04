470a04f53ac1c6bf281f2b8c4e4c7e6c6d40b1d1
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-06-04 16:08:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 # ------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","Ownergit")
+OWNER_USERNAME = getenv("OWNER_USERNAME","imtrigo")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME" , "NexikoBot")
 # --------------------------------------------------------
