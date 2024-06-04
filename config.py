ff746f6b8c2a38296e1bcad4e28331a3e22ff52d
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-06-04 16:20:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SHaikhFurkan45107/TrigoMUSIC",
+    "https://github.com/SHaikhFurkan45107/TrigoMUSIC.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
